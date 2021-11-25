850e24ea0fb2b3c249a5800326afa5aa5a4d2c35
canaelgvin <canaelgvin@gmail.com>
2021-11-25 14:53:58 +0100

    main

--- main.cpp
+++ main.cpp
@@ -7,21 +7,18 @@
 
 using namespace arma;
 
-int ij_to_k(int i,int j);
+int k(int i,int j);
 
 int main(){
 int i, j;
 int M = 5;
 cx_mat u = cx_mat(M-2, M-2, fill::randu);
-cout << u << endl;
 cx_vec U_col = cx_vec(pow(M-2, 2));
-cout << U_col << endl;
-
-cx_vec ncols(u.n_cols);
-cout << ncols << endl;
-
 cx_vec u_vec = vectorise(u);
-cout << u_vec << endl;
+
+cx_vec a = cx_vec(9);
+cx_vec b = cx_vec(9);
+
 
 return 0;
 }
@@ -29,13 +26,4 @@ return 0;
 
 int k(int i,int j){
   return i + n*j
-  }
-
 }
-
-
-// for (i = 0; i <= M - 2; i++){
-//   for (j = 0; j <= M - 2; j++){
-//   U_col[i] += u.col(i,j);
-// }
-// }
