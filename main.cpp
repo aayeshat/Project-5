fab0d0722c43b9fa1bcd2b0314aecf1625f3ebac
canaelgvin <canaelgvin@gmail.com>
2021-11-26 10:34:16 +0100

    commit

--- main.cpp
+++ main.cpp
@@ -13,12 +13,15 @@ int main(){
 int i, j;
 int M = 5;
 cx_mat u = cx_mat(M-2, M-2, fill::randu);
+cout << u << endl;
 cx_vec U_col = cx_vec(pow(M-2, 2));
+cout << U_col << endl;
+
+cx_vec ncols(u.n_cols);
+cout << ncols << endl;
+
 cx_vec u_vec = vectorise(u);
-
-cx_vec a = cx_vec(9);
-cx_vec b = cx_vec(9);
-
+cout << u_vec << endl;
 
 return 0;
 }
@@ -26,4 +29,13 @@ return 0;
 
 int k(int i,int j){
   return i + n*j
+  }
+
 }
+
+
+// for (i = 0; i <= M - 2; i++){
+//   for (j = 0; j <= M - 2; j++){
+//   U_col[i] += u.col(i,j);
+// }
+// }
